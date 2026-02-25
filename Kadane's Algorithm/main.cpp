fe4ff7a4538e3ea6fe59cde4ae23d41f64cd8516
hafizmusanadeem <musanad04@gmail.com>
2026-02-25 23:30:30 +0500

    Bruteforce method of printing subarray sum

--- Kadane's Algorithm/main.cpp
+++ Kadane's Algorithm/main.cpp
@@ -5,21 +5,20 @@ int main(){
     cout<<"     .....Starting the Program....."<<endl;
     int arr[] = {1,2,3,4,5};
     int size = sizeof(arr)/sizeof(int);
-    cout<<"The size of the array is: "<<size<<endl;
+    int max_sum = INT_MIN;
+    cout<<"\nThe size of the array is: "<<size<<endl;
+
     for(int st=0; st<size; st++){
+        int curr_sum = 0;
 
         for(int end=st; end<size; end++){
-            
-            for(int num=st; num<size; num++){
-                
-                cout<<arr[num];
-
-            }
-            cout<<" ";
+            curr_sum += arr[end];
+            max_sum = max(curr_sum,max_sum);
         }
-        cout<<endl;
+        // cout<<endl;
 
     }
+    cout<<"The max subarray sum: "<<max_sum<<endl;
     
     return 0;
 }
